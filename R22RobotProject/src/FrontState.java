6316143f1edc12546760d7215b461e3f1add390f
mandeep543 <75335463+mandeep543@users.noreply.github.com>
2021-03-22 13:14:37 +0000

    Create FrontState.java
    (Realised I didn't push it, sorry!)

---
+++ R22RobotProject/src/FrontState.java
@@ -0,0 +1,10 @@
+
+public class FrontState {
+	
+	private boolean State;
+	
+	public boolean getFrontState() {return State;}
+	
+	public void setFrontState(boolean newValue) {State = newValue;}
+	
+}
