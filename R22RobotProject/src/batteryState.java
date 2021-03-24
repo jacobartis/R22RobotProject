09884f604b19a95c9082aad7459eca47c0080fb2
mandeep543 <75335463+mandeep543@users.noreply.github.com>
2021-03-24 17:31:39 +0000

    Create batteryState.java

---
+++ R22RobotProject/src/batteryState.java
@@ -0,0 +1,14 @@
+public class batteryState {
+    private boolean isLowBattery = false;
+    //sets value to false
+
+    public void setLowBattery(Boolean battery) {
+      isLowBattery = battery;
+    }
+    //setter for LowBattery
+
+    public boolean getLowBattery() {
+      return LowBattery;
+    }
+    //getter for LowBattery
+}
